115f438910965f87a774720bb87c6ff104e1d2e2
Nirmal <88931913+LarawireGarage@users.noreply.github.com>
2024-11-02 04:33:27 +0530

    link blade components dir
    Signed-off-by: Nirmal <88931913+LarawireGarage@users.noreply.github.com>

--- src/BladesServiceProvider.php
+++ src/BladesServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace LarawireGarage\Blades;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class BladesServiceProvider extends ServiceProvider
@@ -24,12 +25,13 @@ class BladesServiceProvider extends ServiceProvider
 
     public function registerConfigs()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../configs/blades.php', 'blades');
+        $this->mergeConfigFrom(__DIR__ . '/../configs/blades.php', 'blades-configs');
         return $this;
     }
     public function registerViews()
     {
-        // $this->loadViewsFrom(__DIR__ . '/../resources/views/components', 'blades');
+        // $this->loadViewsFrom(__DIR__ . '/../resources/views/components/bootstrap', 'bladex');
+        Blade::anonymousComponentPath(__DIR__ . '/../resources/views/components/bootstrap', 'bs');
         return $this;
     }
     public function registerComponents()
@@ -42,6 +44,8 @@ class BladesServiceProvider extends ServiceProvider
     }
     public function publishResources()
     {
+        // $this->publishes([__DIR__ . '/../resources/js/mix' => public_path('vendor/larapex-livewire')], 'larapex-livewire-assets'); // Publish assets
+        // $this->publishes([__DIR__ . '/../configs/larapex-livewire.php' => config_path('larapex-livewire.php')], 'larapex-livewire-configs'); // publish configs
         return $this;
     }
 }
