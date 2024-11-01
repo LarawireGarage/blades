8a6ae02085d709cfaf1e07b7dbd4f1497089f10b
Nirmal <88931913+LarawireGarage@users.noreply.github.com>
2024-11-02 04:34:14 +0530

    Add Bootstrap spinner component
    Signed-off-by: Nirmal <88931913+LarawireGarage@users.noreply.github.com>

--- resources/views/components/bootstrap/spinner.blade.php
+++ resources/views/components/bootstrap/spinner.blade.php
@@ -1,10 +1,10 @@
-@props(['type'=>'border','target' => '', 'mat' => 'none', 'size' => 'sm'])
+@props(['type' => 'border', 'target' => '', 'mat' => 'none', 'size' => 'sm'])
 
 <div {{ $attributes->class([
-    'spinner-'.$type,
-    'spinner-'.$type.'-sm' => strtolower($size) == 'sm',
-    'mr-2' => in_array($mat, ['e', 'end']),
-    'ml-2' => in_array($mat, ['s', 'start']),
+    'spinner-' . $type,
+    'spinner-' . $type . '-sm' => strtolower($size) == 'sm',
+    'me-2' => in_array($mat, ['e', 'end']),
+    'ms-2' => in_array($mat, ['s', 'start']),
     'mx-0' => in_array($mat, ['none', 'false', false, 0]),
 ]) }}
     @if (!empty($target)) wire:loading wire:target='{{ $target }}' @endif role="status">
